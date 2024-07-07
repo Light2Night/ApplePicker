e84f41e47c9438a4cae3eebef2ffd07c13196842
Приходько Владислав <3885vlad@gmail.com>
2024-07-07 13:18:39 +0300

    Apples are spawned in random locations. The apple tree moves in a random direction

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -4,23 +4,50 @@ public class AppleTree : MonoBehaviour {
 	[Header("Set in Inspector")]
 	public GameObject applePrefab;
 	public float speed = 1f;
-	public float leftAndRightEdge = 10f;
-	public float chanceToChangeDirections = 0.1f;
+	public float leftAndRightEdge = 2f;
+	public float upAndDownEdge = 1f;
+	public float secondsToChangeDirections = 2f;
 	public float secondsBetweenAppleDrops = 1f;
+	public float treeHeightOffsetForApple = 3F;
+	private Vector3 direction;
 
 	// Start is called before the first frame update
 	void Start() {
-		InvokeRepeating(nameof(SpawnApple), secondsBetweenAppleDrops, secondsBetweenAppleDrops);
+		SetNewRandonDirection();
+
+		InvokeRepeating(nameof(CreateApple), secondsBetweenAppleDrops, secondsBetweenAppleDrops);
+		InvokeRepeating(nameof(SetNewRandonDirection), secondsToChangeDirections, secondsToChangeDirections);
 	}
 
 	// Update is called once per frame
 	void Update() {
 		Vector3 pos = transform.position;
-		pos.x += speed * Time.deltaTime;
+		pos += direction * Time.deltaTime;
 		transform.position = pos;
 	}
 
-	private void SpawnApple() {
-		Instantiate(applePrefab, transform.position, transform.rotation);
+	private void CreateApple() {
+		var position = transform.position;
+
+		position.y += treeHeightOffsetForApple + Random.Range(-upAndDownEdge, upAndDownEdge);
+		position.x += Random.Range(-leftAndRightEdge, leftAndRightEdge);
+		position.z += Random.Range(-leftAndRightEdge, leftAndRightEdge);
+
+		Instantiate(applePrefab, position, transform.rotation);
+	}
+
+	Vector3 GetRandomHorizontalDirection() {
+		float x = Random.Range(-1f, 1f);
+		float z = Random.Range(-1f, 1f);
+
+		Vector3 direction = new Vector3(x, 0f, z);
+
+		direction.Normalize();
+
+		return direction;
+	}
+
+	private void SetNewRandonDirection() {
+		direction = GetRandomHorizontalDirection();
 	}
 }
